b23323853077a5de4be599caa6b65d52360e003c
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-30 20:42:21 +1000

    temp for serach

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use termgame::{Controller, CharChunkMap, GameSettings, Game, GameEvent, SimpleEvent, KeyCode, run_game};
+use termgame::{
+    run_game, CharChunkMap, Controller, Game, GameEvent, GameSettings, KeyCode, SimpleEvent,
+};
 
 use std::collections::HashMap;
 use std::error::Error;
 use std::time::Duration;
-
 
 /// This is a single "buffer".
 struct Buffer {
@@ -19,7 +20,7 @@ impl Buffer {
     fn new(file: Option<String>) -> Buffer {
         Buffer {
             text: String::new(),
-            file
+            file,
         }
     }
 
@@ -67,7 +68,6 @@ impl Buffer {
     // fn example_ref(&self) -> i32 {
     //     todo!()
     // }
-
 }
 
 /// This struct implements all the
@@ -80,60 +80,54 @@ struct BufferEditor {
 
 impl Controller for BufferEditor {
     /// This gets run once, you can probably ignore it.
-    fn on_start(&mut self, _game: &mut Game) {
-    }
+    fn on_start(&mut self, _game: &mut Game) {}
 
     /// Any time there's a keypress, you'll get this
     /// function called.
     fn on_event(&mut self, game: &mut Game, event: GameEvent) {
         match event.into() {
-            SimpleEvent::Just(KeyCode::Char(c)) => {
-                self.buffer.push_char(c)
-            },
-            SimpleEvent::Just(KeyCode::Enter) => {
-                self.buffer.push_char('\n')
-            },
+            SimpleEvent::Just(KeyCode::Char(c)) => self.buffer.push_char(c),
+            SimpleEvent::Just(KeyCode::Enter) => self.buffer.push_char('\n'),
             SimpleEvent::Just(KeyCode::Esc) => {
                 game.end_game();
-            },
+            }
             SimpleEvent::Just(KeyCode::Up) => {
                 let mut viewport = game.get_viewport();
                 if viewport.y > 0 {
                     viewport.y -= 1;
                 }
                 game.set_viewport(viewport)
-            },
+            }
             SimpleEvent::Just(KeyCode::Down) => {
                 let mut viewport = game.get_viewport();
                 viewport.y += 1;
                 game.set_viewport(viewport)
-            },
+            }
             SimpleEvent::WithControl(KeyCode::Char('s')) => {
                 // Implement
                 println!("Saving...");
                 // todo!()
-            },
+            }
             // This is bad binding
             SimpleEvent::WithControl(KeyCode::Char('f')) => {
                 // Clear entire line
                 loop {
                     match self.buffer.pop_char() {
                         Some('\n') => break,
-                        Some(_) => {},
+                        Some(_) => {}
                         None => break,
                     }
                 }
-            },
+            }
             SimpleEvent::Just(KeyCode::Backspace) => {
                 self.buffer.pop_char();
-            },
+            }
 
             _ => {}
         }
         let mut chunkmap = CharChunkMap::new();
         self.buffer.chunkmap_from_textarea(&mut chunkmap);
         game.swap_chunkmap(&mut chunkmap);
-
     }
 
     /// This function gets called regularly, so you can use it
@@ -142,20 +136,19 @@ impl Controller for BufferEditor {
     fn on_tick(&mut self, _game: &mut Game) {}
 }
 
-fn run_command(cmd: &str, editors: &mut HashMap<String, BufferEditor>)  -> Result<(), Box<dyn Error>> {
-
+fn run_command(
+    cmd: &str,
+    editors: &mut HashMap<String, BufferEditor>,
+) -> Result<(), Box<dyn Error>> {
     let mut buffer_uiids: i32 = 0;
 
     match first_word(cmd) {
         "open" => run_game(
             fetch_editor(editors, cmd, &mut buffer_uiids),
-            GameSettings::new()
-                .tick_duration(Duration::from_millis(25))
+            GameSettings::new().tick_duration(Duration::from_millis(25)),
         )?,
-        "search" => {
-            print_buffer_searches(editors, cmd)
-        }
-        _ => println!("Command not recognised")
+        "search" => print_buffer_searches(editors, cmd),
+        _ => println!("Command not recognised"),
     }
     // if cmd.starts_with("open") {
     //     run_game(
@@ -171,7 +164,6 @@ fn run_command(cmd: &str, editors: &mut HashMap<String, BufferEditor>)  -> Resul
 }
 
 fn print_buffer_searches(editors: &HashMap<String, BufferEditor>, cmd: &str) {
-
     let first_word = first_word(cmd);
     let index_rest = first_word.len() - 1;
     let search_term = &cmd[index_rest..];
@@ -179,13 +171,13 @@ fn print_buffer_searches(editors: &HashMap<String, BufferEditor>, cmd: &str) {
     for editor in editors.values() {
         for line in editor.buffer.text.lines() {
             if line.contains(search_term) {
-                println!("{}", line);
-            }
+                println!("found: {}", line);
+            };
         }
     }
 }
 
-fn first_word (s: &str) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -197,7 +189,11 @@ fn first_word (s: &str) -> &str {
     s
 }
 
-fn fetch_editor<'a> (editors: &'a mut HashMap<String, BufferEditor>, cmd: &str, uuid: &mut i32) -> &'a mut BufferEditor {
+fn fetch_editor<'a>(
+    editors: &'a mut HashMap<String, BufferEditor>,
+    cmd: &str,
+    uuid: &mut i32,
+) -> &'a mut BufferEditor {
     let args: Vec<&str> = cmd.split_whitespace().collect();
     let buffer_name: String = match args.get(1) {
         Some(name) => String::from(*name),
@@ -213,15 +209,15 @@ fn fetch_editor<'a> (editors: &'a mut HashMap<String, BufferEditor>, cmd: &str, 
         }
     };
 
-    editors.entry(buffer_name).or_insert(BufferEditor { buffer: Buffer::new(None) })
+    editors.entry(buffer_name).or_insert(BufferEditor {
+        buffer: Buffer::new(None),
+    })
 }
-
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     println!("Welcome to BuffeRS. ");
 
     let mut editors: HashMap<String, BufferEditor> = HashMap::new();
@@ -234,17 +230,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             Ok(line) => {
                 run_command(&line, &mut editors)?;
                 rl.add_history_entry(line.as_str());
-            },
-            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
-                break
-            },
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
             Err(err) => {
                 println!("Error: {:?}", err);
-                break
+                break;
             }
         }
     }
-
 
     Ok(())
 }
